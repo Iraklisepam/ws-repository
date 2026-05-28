fffcec4f0ce94c5bf1da1379a99b6dc7ea6359e8
Giorgi <tserediani.g@gmail.com>
2026-05-28 15:52:02 +0400

    feat: add sqlite functionality including repository and table initialization

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import sqlite3
 from dataclasses import dataclass
 from typing import Protocol
 from repos.in_memory import InMemoryRepository
 from repos.json_repo import JsonRepository
+from repos.sqlite_repo import SQLiteRepository
+
 
 @dataclass
 class User:
@@ -27,11 +30,31 @@ class UserService:
         return User(**user_data) if user_data else None
 
 
+def _init_db(conn: sqlite3.Connection) -> None:
+    cursor = conn.cursor()
+    query = """
+        CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY, 
+        name TEXT,
+        email TEXT UNIQUE,
+        age INTEGER 
+        )
+"""
+
+    cursor.execute(query)
+
+
 if __name__ == "__main__":
     # in_memory_repo = InMemoryRepository()
     # user_service = UserService(in_memory_repo)
-    json_repo = JsonRepository("users.json")
-    user_service = UserService(json_repo)
-    user1 = User("Alice", "alice@example.com", 30)
-    user_service.register(user1)
-    print(user_service.find_by_email("alice@example.com"))
+
+    # json_repo = JsonRepository("users.json")
+    # user_service = UserService(json_repo)
+    with sqlite3.connect('users.db') as conn:
+        _init_db(conn)
+        sqlite_repo = SQLiteRepository(conn)
+        user_service = UserService(sqlite_repo)
+
+        user1 = User("Alice", "alice@example.com", 30)
+        user_service.register(user1)
+        print(user_service.find_by_email("alice@example.com"))
